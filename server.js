89ed29b58320b656dd37a3cbff0b6b9fa3cd171b
deepakrana123 <49833009+deepakrana123@users.noreply.github.com>
2020-11-07 21:41:22 -0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express =require('express');
 const http=require('http');
 const path=require('path');
 const app=express();
-const port=6000;
+const port=process.env.PORT||6000;;
 
 
 app.use(express.static(path.join(__dirname,'build')));
